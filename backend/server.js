00ef20681bbea739a33e0fa3b8464cc56718e411
Nikita Derevskov <nikitaderevskov@gmail.com>
2020-11-08 02:27:11 +0300

    backend: add configurate app block

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ const errorHandler = require('errorHandler');
 
 const app = express();
 
+/*  Configurate app */
+
+app.use(cors()); // Для работы с cors
+app.use(require('morgan')('dev')); // Для работы с логированием
+app.use(bodyParser.urlencoded({ extended: false })); // для работы post запросами где строки или массивы
+app.use(bodyParser.json()); // для работы с post запросами формата json
+app.use(express.static(path.join(__dirname, 'public'))); // для работы с файловой системой, конкретно тут - куда кидать статику
+app.use(session({secret: 'jwt-auth', cookie:  {maxAge: 6000}, resave: false, saveUninitialized: false})); // для работы с сессиями (maxAge - длительность жизни)
+
+/*  */
 
 app.use((err, req, res) => {
 	res.status(err.status | 500);
